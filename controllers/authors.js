9e00b12b114faf7d59528548092888d6f312c6bb
Christine Samarchi <christinesamarchi@Christines-MacBook-Air.local>
2018-10-15 12:47:44 -0500

    show ejs page works

--- controllers/authors.js
+++ controllers/authors.js
@@ -3,7 +3,7 @@ const router = express.Router();
 //Models job is  to communicate with the DB
 const Author = require('../models/authors')
 
-
+//index route
 router.get('/', (req, res) => {
   Author.find({}, (err, foundAuthors) => {
     res.render('authors/index.ejs', {
@@ -12,33 +12,53 @@ router.get('/', (req, res) => {
   })
 })
 
-router.get('/', (req, res) => {
-  res.render('authors/index.ejs');
-})
-
+//new route
 router.get('/new', (req, res) => {
   res.render('authors/new.ejs');
 })
 
+//show route
+router.get('/:index', (req, res) => {
+  Author.findById(req.params.index, (err, authorFound) => {
+    res.render('authors/show.ejs', {
+      author: authorFound
+    })
+  })
+})
 
+//edit
+router.get('/:index/edit', (req, res) => {
+  Author.findById(req.params.index, (err, editAuthor) => {
+    res.render('authors/edit.ejs', {
+      author: editAuthor
+    });
+  });
+});
+
+//post route
 router.post('/', (req, res) => {
-
   Author.create(req.body, (err, createdAuthor) => {
-
     if(err){
       console.log(err)
     } else {
       res.redirect('/authors')
     }
   });
-
 });
 
-router.get('/authors/:index', (req, res) => {
-  Authors.findById(req.params.index, (err, authorFound) => {
-    res.render('authors/show.ejs', {
-      author: authorFound
-    })
+router.put('/:index', (req, res) => {
+  Author.findByIdAndUpdate(req.params.id, req.body, (err, updateBody) => {
+    res.redirect('/authors');
+  })
+})
+
+router.delete('/:index', (req, res) => {
+  Author.findByIdAndRemove(req.params.index, (err, deleted) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect('/authors')
+    }
   })
 })
 
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express    = require('express');
 const app        = express();
 const bodyParser = require('body-parser');
-
+const methodOverride = require('method-override');
 require('./db/db');
 
 const authorsController = require('./controllers/authors');
 
 //middleware- must require this before our controller
 app.use(bodyParser.urlencoded({extended: false}));
-
+app.use(methodOverride('_method'));
 
 app.use('/authors', authorsController)
 
